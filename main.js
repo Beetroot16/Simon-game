e26dc2842fadd6a23ca6d7a0cef5587d361779a2
Beetroot16 <vishrut.deshmukh@somaiya.edu>
2022-06-07 22:09:38 +0530

    Yayyy its donee

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
+var gamePattern = []
+var userPattern = []
+level = 0
+var started = false;
+var buttonColors = ["red", "green", "blue", "yellow"]
+
 function next_sequence(){
+    userPattern = []
+    level++
+    $("h1").html("level " + level)
     var randomNumber = Math.floor(Math.random()*4) + 1
         if (randomNumber == 1){
             randomColor = buttonColors[0]
@@ -18,54 +27,60 @@ function next_sequence(){
             $(".yellow").fadeIn(200).fadeOut(200).fadeIn(200)
         }
         gamePattern.push(randomColor)
-        console.log(gamePattern)
+        console.log("game pattern = ", gamePattern)
 }
 
-function handler(){
-    $(".red").click(function(){
-        $(".red").fadeIn(100).fadeOut(100).fadeIn(100)
-        userPattern.push("red")
-        console.log(userPattern)
-    })
-    $(".green").click(function(){
-        $(".green").fadeIn(100).fadeOut(100).fadeIn(100)
-        userPattern.push("green")
-        console.log(userPattern)
-    })
-    $(".blue").click(function(){
-        $(".blue").fadeIn(100).fadeOut(100).fadeIn(100)
-        userPattern.push("blue")
-        console.log(userPattern)
-    })
-    $(".yellow").click(function(){  
-        $(".yellow").fadeIn(100).fadeOut(100).fadeIn(100)
-        userPattern.push("yellow")
-        console.log(userPattern)
-    })   
-}
-
-function checkans(level,gamePattern,userPattern){
-    if (JSON.stringify(userPattern)==JSON.stringify(gamePattern)){
-        console.log("success")
-        userPattern = []
-        return true
+function checkans(level){
+    if (userPattern[level] === gamePattern[level]){
+        if (userPattern.length === gamePattern.length){
+            setTimeout(function(){
+                console.log("success")
+                next_sequence();
+            },1000);
+    }
     }
     else{
         console.log("fail")
-        userPattern = []
-        return false
+        $("h1").html("GAME OVER !")
     }
-    
-    level += 1 
 }
 
-var gamePattern = []
-var userPattern = []
-level = 0
-var buttonColors = ["red", "green", "blue", "yellow"]
-$(document).keypress(function(){
-    $("h1").text("Level " + level+1)
-    next_sequence()
-    handler()
-    setTimeout(function(){checkans(level,gamePattern,userPattern);}, 2000);    
+function restart(){
+    level = 0
+    gamePattern = []
+    started = false
+}
+
+$(".red").click(function(){
+    $(".red").fadeIn(100).fadeOut(100).fadeIn(100)
+    userPattern.push("red")
+    checkans(userPattern.length - 1);
+    console.log("user pattern = ", userPattern) 
 })
+$(".green").click(function(){
+    $(".green").fadeIn(100).fadeOut(100).fadeIn(100)
+    userPattern.push("green")
+    checkans(userPattern.length - 1);
+    console.log("user pattern = ", userPattern) 
+})
+$(".blue").click(function(){
+    $(".blue").fadeIn(100).fadeOut(100).fadeIn(100)
+    userPattern.push("blue")
+    checkans(userPattern.length - 1);
+    console.log("user pattern = ", userPattern) 
+})
+$(".yellow").click(function(){  
+    $(".yellow").fadeIn(100).fadeOut(100).fadeIn(100)
+    userPattern.push("yellow")
+    checkans(userPattern.length - 1);
+    console.log("user pattern = ", userPattern) 
+})  
+
+$(document).keydown(function() {
+    if(!started){
+    $("h1").html("level " + level);
+    next_sequence();
+    started = true;
+  }
+  });
+
